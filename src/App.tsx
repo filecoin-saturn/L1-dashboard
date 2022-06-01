7acdc8a0204510f9a462b1c8d7f94118210e7ab4
Eric Guan <guanzo91@gmail.com>
2022-05-31 20:26:47 -0700

    feat: setup e2e tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Saturn L2 Web UI</p>
-      </header>
-    </div>
-  )
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Saturn L2 Web UI</p>
+            </header>
+        </div>
+    )
 }
 
 export default App
---
+++ test/e2e/App.test.tsx
@@ -0,0 +1,33 @@
+import { afterAll, beforeAll, describe, expect, test } from 'vitest'
+import { preview } from 'vite'
+import type { PreviewServer } from 'vite'
+import puppeteer from 'puppeteer'
+import type { Browser, Page } from 'puppeteer'
+
+describe('basic', async () => {
+    let server: PreviewServer
+    let browser: Browser
+    let page: Page
+
+    beforeAll(async () => {
+        server = await preview({ preview: { port: 3000 } })
+        browser = await puppeteer.launch()
+        page = await browser.newPage()
+    })
+
+    afterAll(async () => {
+        await browser.close()
+        await new Promise<void>((resolve, reject) => {
+            server.httpServer.close(error => error ? reject(error) : resolve())
+        })
+    })
+
+    test('should have the correct title', async () => {
+        await page.goto('http://localhost:3000')
+        const header = (await page.$('.App-header'))!
+        expect(header).toBeDefined()
+
+        let text = await page.evaluate(header => header.textContent, header)
+        expect(text).toBe('Saturn L2 Web UI')
+    }, 60_000)
+})
