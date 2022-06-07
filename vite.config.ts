a78475acd766ce0656c375688042f5388d7edde4
Eric Guan <guanzo91@gmail.com>
2022-06-06 20:43:37 -0700

    fix: assets base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: {
-        // saturn-l2 depends on this path
-        assetsDir: 'webui'
-    },
+    // saturn-l2 depends on this path
+    base: '/webui',
     plugins: [react()],
     server: {
         port: 3010,
