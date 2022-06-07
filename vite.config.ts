0ad5e7fa0a56b8582c2b464cd0ab9b939051cd05
Eric Guan <guanzo91@gmail.com>
2022-06-06 20:31:57 -0700

    feat: update assets path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        // saturn-l2 depends on this path
+        assetsDir: 'webui'
+    },
     plugins: [react()],
     server: {
         port: 3010,
