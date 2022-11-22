87e7c332c5c7147ac9c50f5ba65dcda28ca5d485
Karol Wypchlo <kwypchlo@gmail.com>
2022-11-22 15:48:59 +0100

    get around strict aws cors settings on local dev server

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,33 @@
 import * as path from 'path'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
     // saturn-l2 depends on this path
     base: '/webui/',
     plugins: [react()],
     server: {
         port: 3010,
-        strictPort: true
+        strictPort: true,
+        // proxy for aws metrics endpoint to get around strict cors settings when developing locally
+        proxy: {
+          "/metrics": {
+            target: env.VITE_METRICS_ORIGIN ?? "https://ln3tnkd4d5uiufjgimi6jlkmci0bceff.lambda-url.us-west-2.on.aws/",
+            changeOrigin: true,
+            secure: false,
+            rewrite: (path) => path.replace(/^\/metrics/, ""),
+          },
+        },
     },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src')
         }
     }
+  }
 })
