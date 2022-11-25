58ec2b650feb16dc25490e68f1469ef64d43a7dc
Karol Wypchlo <kwypchlo@gmail.com>
2022-11-25 13:04:34 +0100

    include prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,28 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: [
-        'plugin:@typescript-eslint/recommended',
-        'react-app',
-        'standard',
-        'plugin:jsx-a11y/recommended'
-    ],
-    plugins: ['@typescript-eslint', 'jsx-a11y'],
-    rules: {
-        indent: ['error', 4],
-        'jsx-a11y/no-autofocus': 'off',
-        'spaced-comment': 'warn'
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "react-app",
+    "standard",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint", "jsx-a11y"],
+  rules: {
+    "jsx-a11y/no-autofocus": "off",
+  },
+  globals: {
+    cl: true,
+  },
+  // https://github.com/eslint/typescript-eslint-parser/issues/437#issuecomment-435526531
+  overrides: [
+    {
+      files: ["**/*.ts"],
+      parser: "@typescript-eslint/parser",
+      rules: {
+        "no-undef": "off",
+        "no-unused-vars": "off",
+      },
     },
-    globals: {
-        cl: true
-    },
-    // https://github.com/eslint/typescript-eslint-parser/issues/437#issuecomment-435526531
-    overrides: [
-        {
-            files: ['**/*.ts'],
-            parser: '@typescript-eslint/parser',
-            rules: {
-                'no-undef': 'off',
-                'no-unused-vars': 'off'
-            }
-        }
-    ]
-}
+  ],
+};
